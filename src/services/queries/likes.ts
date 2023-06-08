0135c3f4f7db3306380944ee2f67e9651f2f65a7
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-06-08 20:34:00 +0330

    implemented like and unlike item

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,28 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { itemsKey, userLikesKey } from "$services/keys";
+import { client } from "$services/redis";
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+};
+
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
+
+    if (inserted) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+
+    if (removed) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
