221d600303f456a112700152b21737c4288c91b3
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-06-09 15:07:14 +0330

    improved create item - added item to sorted set

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,4 +1,4 @@
-import { itemsKey } from '$services/keys';
+import { itemsByViewKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
 import { genId } from '$services/utils';
@@ -36,7 +36,13 @@ export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 
     const serialized = serialize(attrs);
 
-    await client.hSet(itemsKey(id), serialized);
+    await Promise.all([
+        client.hSet(itemsKey(id), serialized),
+        client.zAdd(itemsByViewKey(), {
+            value: id,
+            score: 0
+        })
+    ]);
 
     return id;
 };
