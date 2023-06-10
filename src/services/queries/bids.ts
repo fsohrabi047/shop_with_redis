8794479f979cad20a34ea2149bfac8f11144b729
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-06-10 17:37:13 +0330

    improved create bid feature - added pipeline

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,4 +1,4 @@
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { DateTime } from 'luxon';
@@ -21,7 +21,14 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 
 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+	return Promise.all([
+		client.rPush(bidHistoryKey(attrs.itemId), serialized),
+		client.hSet(itemsKey(item.id), {
+			bids: item.bids + 1,
+			price: attrs.amount,
+			highestBidUserId: attrs.userId
+		})
+	]);
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
