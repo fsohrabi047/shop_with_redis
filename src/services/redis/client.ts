fa787c8721205f32e1ebca5b1cb1f00d81118041
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-06-16 15:50:49 +0330

    implemented full text search

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,5 +1,6 @@
 import { itemsByViewKey, itemsKey, itemsViewsKey } from '$services/keys';
 import { createClient, defineScript } from 'redis';
+import { createIndexes } from './create-indexes';
 
 const client = createClient({
 	socket: {
@@ -60,5 +61,12 @@ const client = createClient({
 
 client.on('error', (err) => console.error(err));
 client.connect();
+client.on('connect',async () => {
+	try {
+		await createIndexes();
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 export { client };
